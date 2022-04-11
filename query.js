b6b1b74dc84f87a1b5f989a91123fff5912155b2
Jenna Leopold <jleopold424@gmail.com>
2022-04-11 15:00:17 -0400

    completed lesson

--- query.js
+++ query.js
@@ -6,9 +6,16 @@ const { User, Task, sequelize } = require('./models')
 const findAllWithTasks = async () => {
   // Find all users with their associated tasks
   // Raw SQL: SELECT * FROM users JOIN tasks ON tasks."userId" = users.id;
+  const users = await User.findAll({ include: [Task] })
+  console.log(JSON.stringify(users, null, 2))
 }
 
 const findAllJohnsWithTasks = async () => {
+  const johns = await User.findOne({
+    where: { firstName: 'John' },
+    include: [Task]
+  })
+  console.log(JSON.stringify(johns, null, 2))
   // Find all users with their associated tasks
   // Raw SQL: SELECT * FROM users JOIN tasks ON tasks."userId" = users.id WHERE users."firstName" = 'John';
 }
